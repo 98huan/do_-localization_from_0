446c3c21b876aa06db5d61d4f49adfa461595024
98huan <1143016001@qq.com>
2022-11-09 17:46:52 +0800

    gps画轨迹

--- lidar_localization/src/test_frame_node.cpp
+++ lidar_localization/src/test_frame_node.cpp
@@ -1,98 +1,86 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: Ren Qian
  * @Date: 2020-02-05 02:56:27
  */
 #include <ros/ros.h>
-#include <pcl/common/transforms.h>
 #include "glog/logging.h"
+#include <sys/stat.h>
 
 #include "lidar_localization/global_defination/global_defination.h"
-#include "lidar_localization/subscriber/cloud_subscriber.hpp"
-#include "lidar_localization/subscriber/imu_subscriber.hpp"
 #include "lidar_localization/subscriber/gnss_subscriber.hpp"
-#include "lidar_localization/tf_listener/tf_listener.hpp"
-#include "lidar_localization/publisher/cloud_publisher.hpp"
-#include "lidar_localization/publisher/odometry_publisher.hpp"
-
+#include <nav_msgs/Path.h>
+#include <math.h>
 using namespace lidar_localization;
 
-int main(int argc, char *argv[]) {
+//全局变量
+ros::Publisher gps_path_pub_;
+nav_msgs::Path gps_path;
+geometry_msgs::PoseStamped pose;
+
+int main(int argc, char *argv[])
+{
     google::InitGoogleLogging(argv[0]);
     FLAGS_log_dir = WORK_SPACE_PATH + "/Log";
-    FLAGS_alsologtostderr = 1;
+    FLAGS_alsologtostderr = 1;  //让日志同时输出到终端
+    if (access(FLAGS_log_dir.data(), F_OK) < 0) {
+        LOG_IF(FATAL, (mkdir(FLAGS_log_dir.data(), S_IRWXU) < 0)) << "create folder error: " << FLAGS_log_dir;
+    }
 
     ros::init(argc, argv, "test_frame_node");
     ros::NodeHandle nh;
 
-    std::shared_ptr<CloudSubscriber> cloud_sub_ptr = std::make_shared<CloudSubscriber>(nh, "/kitti/velo/pointcloud", 100000);
-    std::shared_ptr<IMUSubscriber> imu_sub_ptr = std::make_shared<IMUSubscriber>(nh, "/kitti/oxts/imu", 1000000);
-    std::shared_ptr<GNSSSubscriber> gnss_sub_ptr = std::make_shared<GNSSSubscriber>(nh, "/kitti/oxts/gps/fix", 1000000);
-    std::shared_ptr<TFListener> lidar_to_imu_ptr = std::make_shared<TFListener>(nh, "velo_link", "imu_link");
+    std::shared_ptr<GNSSSubscriber> gnss_sub_ptr = std::make_shared<GNSSSubscriber>(nh, "/odometry/gpsz", 1000000);
 
-    std::shared_ptr<CloudPublisher> cloud_pub_ptr = std::make_shared<CloudPublisher>(nh, "current_scan", 100, "map");
-    std::shared_ptr<OdometryPublisher> odom_pub_ptr = std::make_shared<OdometryPublisher>(nh, "lidar_odom", "map", "lidar", 100);
+    ros::Publisher gps_start_point_pub_ = nh.advertise<sensor_msgs::NavSatFix>("/ref_point_wgs84", 100, "map");
 
-    std::deque<CloudData> cloud_data_buff;
-    std::deque<IMUData> imu_data_buff;
     std::deque<GNSSData> gnss_data_buff;
-    Eigen::Matrix4f lidar_to_imu = Eigen::Matrix4f::Identity();
-    bool transform_received = false;
+
     bool gnss_origin_position_inited = false;
 
-    ros::Rate rate(100);
-    while (ros::ok()) {
-        ros::spinOnce();
+    sensor_msgs::NavSatFix gps_start_point; //gps起点
+    gps_path_pub_ = nh.advertise<nav_msgs::Path>("gps_path", 10);
 
-        cloud_sub_ptr->ParseData(cloud_data_buff);
-        imu_sub_ptr->ParseData(imu_data_buff);
+    ros::Rate rate(100);
+    while (ros::ok())
+    {
+        ros::spinOnce();
         gnss_sub_ptr->ParseData(gnss_data_buff);
 
-        if (!transform_received) {
-            if (lidar_to_imu_ptr->LookupData(lidar_to_imu)) {
-                transform_received = true;
-                // LOG(INFO) << "lidar to imu transform matrix is:" << std::endl << lidar_to_imu;
+        while (gnss_data_buff.size() > 0)
+        {
+            GNSSData gnss_data = gnss_data_buff.front();
+            gnss_data_buff.pop_front();
+
+            // gps初始化
+            if (!gnss_origin_position_inited)
+            {
+                gnss_data.InitOriginPosition();
+
+                gps_start_point.header.stamp = ros::Time::now();
+                gps_start_point.header.frame_id = "map";
+                gps_start_point.latitude = gnss_data.latitude;
+                gps_start_point.longitude = gnss_data.longitude;
+                gps_start_point.altitude = gnss_data.altitude;
+                gps_start_point.status.service = 0;
+
+                gnss_origin_position_inited = true;
             }
-        } else {
-            while (cloud_data_buff.size() > 0 && imu_data_buff.size() > 0 && gnss_data_buff.size() > 0) {
-                CloudData cloud_data = cloud_data_buff.front();
-                IMUData imu_data = imu_data_buff.front();
-                GNSSData gnss_data = gnss_data_buff.front();
+            // gps相对位置更新
+            gnss_data.UpdateXYZ();
 
-                double d_time = cloud_data.time - imu_data.time;
-                if (d_time < -0.05) {
-                    cloud_data_buff.pop_front();
-                } else if (d_time > 0.05) {
-                    imu_data_buff.pop_front();
-                    gnss_data_buff.pop_front();
-                } else {
-                    cloud_data_buff.pop_front();
-                    imu_data_buff.pop_front();
-                    gnss_data_buff.pop_front();
-
-                    Eigen::Matrix4f odometry_matrix;
-
-                    if (!gnss_origin_position_inited) {
-                        gnss_data.InitOriginPosition();
-                        gnss_origin_position_inited = true;
-                    }
-                    gnss_data.UpdateXYZ();
-                    odometry_matrix(0,3) = gnss_data.local_E;
-                    odometry_matrix(1,3) = gnss_data.local_N;
-                    odometry_matrix(2,3) = gnss_data.local_U;
-                    odometry_matrix.block<3,3>(0,0) = imu_data.GetOrientationMatrix();
-                    odometry_matrix *= lidar_to_imu;
-
-                    pcl::transformPointCloud(*cloud_data.cloud_ptr, *cloud_data.cloud_ptr, odometry_matrix);
-
-                    cloud_pub_ptr->Publish(cloud_data.cloud_ptr);
-                    odom_pub_ptr->Publish(odometry_matrix);
-                }
-            }
+            gps_path.header.frame_id = "map";
+            gps_path.header.stamp = ros::Time::now();
+            pose.header = gps_path.header;
+            pose.pose.position.x = gnss_data.local_E;
+            pose.pose.position.y = gnss_data.local_N;
+            pose.pose.position.z = gnss_data.local_U;
+            gps_path.poses.push_back(pose);
+            LOG(INFO) << gnss_data.local_E << "," << gnss_data.local_N << "," << gnss_data.local_U;
+            gps_start_point_pub_.publish(gps_start_point);
+            gps_path_pub_.publish(gps_path);
         }
-
         rate.sleep();
     }
-
     return 0;
 }
